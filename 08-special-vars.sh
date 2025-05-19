7f2943f772007d924346da56500eade86961d7f4
karna <karunakark1729@gmail.com>
2025-05-19 21:52:46 +0530

    Special Variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "All variables passed to the script: $2"
+echo "All variables passed to the script: $@"
+echo "Number of variables: $#"
+echo "Script Name: $0"
+echo "Current Directory: $PWD"
+echo "User running the Script: $USER"
+echo "Home Direcotry of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of lat command in background: $!"
