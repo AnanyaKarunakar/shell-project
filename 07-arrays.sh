cb1f7e8631900b9435ee3501ff6c00354710bad3
karna <karunakark1729@gmail.com>
2025-05-19 16:39:20 +0530

    Arrays

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,4 +4,5 @@ MOVIES=("Court" "HIT3" "Pushpa2" "Devara")
 
 echo "First Movie: ${MOVIES[0]}"
 echo "Third Movie: ${MOVIES[3]}"
+echo "Fourth Movie: ${MOVIES[4]}"  # If i took unavailable element
 echo "All Movies: ${MOVIES[@]}"
