ab887de967b5cceeaf58b29e7b6b685516b70ee6
karna <karunakark1729@gmail.com>
2025-05-19 16:36:32 +0530

    Arrays

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Movies=("Court" "HIT3" "Pushpa2" "Devara")
+MOVIES=("Court" "HIT3" "Pushpa2" "Devara")
 
 echo "First Movie: ${MOVIES[0]}"
 echo "Third Movie: ${MOVIES[3]}"
-echo "All Movie: ${MOVIES[@]}"
+echo "All Movies: ${MOVIES[@]}"
