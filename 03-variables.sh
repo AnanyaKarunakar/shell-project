9a2f812e76e9841ea3d372a8e650b7bd171ac438
karna <karunakark1729@gmail.com>
2025-05-19 13:20:44 +0530

    Conversation by using Variables

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"   		# to differ normal text and variable that why we represent variable with $
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am fine too. What's up?"
+echo "$PERSON2:: Nothing, Just going to Mars now, will you come"
+echo "$PERSON1:: Sorry, you carry on! I will come once yu come back"
+echo "$PERSON2:: Ho thagt's great Idea"
+echo "$PERSON1:: Sure!!
