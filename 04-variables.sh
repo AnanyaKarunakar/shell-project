5b408f781430806c7454456928c68d133517bc7f
karna <karunakark1729@gmail.com>
2025-05-19 15:50:38 +0530

    Conversation by using Variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Modi  # there is no space before and after =
-PERSON2=Musk
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Hey $PERSON2, How are you?"   		# to differ normal text and variable that why we represent variable with $
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing?"
