0ae32efe74b7d5e951766f6e5e6b56f37af956ac
karna <karunakark1729@gmail.com>
2025-05-22 20:43:19 +0530

    Functions to install mysql, python3, nginx

--- 11-functions.sh
+++ 11-functions.sh
@@ -10,7 +10,7 @@ else
 fi
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-	if [ $? -eq 0 ]
+	if [ $1 -eq 0 ]
 	then
 		echo "Installing $2 is ... SUCCESS"
 	else
