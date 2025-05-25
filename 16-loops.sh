7292b2d4c12920c8b1526ede7ab323cadea97741
karna <karunakark1729@gmail.com>
2025-05-25 12:00:18 +0530

    Loops

--- 16-loops.sh
+++ 16-loops.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-PACKAGES=("mysql" "python3" "nginx" "httpd")
+PACKAGES=("mysql" "python" "nginx" "httpd")
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
@@ -30,15 +30,16 @@ VALIDATE() {
     fi
 }
 
-for packages in ${PACKAGES[@]}; do
+for package in ${PACKAGES[@]}
+do
     dnf list installed $package &>>$LOG_FILE
 
     if [ $? -ne 0 ]; then
-        echo "$PACKAGES is not installed... going to install it" | tee -a $LOG_FILE
-        dnf install $PACKAGES -y &>>$LOG_FILE
-        VALIDATE $? "$PACKAGES"
+        echo "$package is not installed... going to install it" | tee -a $LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
     else
-        echo -e "Nothing to do $PACKAGES... $Y already installed $N" | tee -a $LOG_FILE
+        echo -e "Nothing to do $package... $Y already installed $N" | tee -a $LOG_FILE
     fi
 
 done
