55c66d0d8c2f567b2d1279e699c5fa834c31f1fa
karna <karunakark1729@gmail.com>
2025-08-11 18:18:05 +0530

    06

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,4 +1,3 @@
-#Data types
 #!/bin/bash
 
 NUMBER1=100
@@ -15,13 +14,13 @@ echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
 
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+# PERSON1=$1
+# PERSON2=$2
 
-echo "$PERSON1:: Hey $PERSON2, How are you?"   		# to differ normal text and variable that why we represent variable with $
-echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing?"
-echo "$PERSON1:: I am fine too. What's up?"
-echo "$PERSON2:: Nothing, Just going to Mars now, will you come"
-echo "$PERSON1:: Sorry, you carry on! I will come once yu come back"
-echo "$PERSON2:: Ho thagt's great Idea"
-echo "$PERSON1:: Sure!!"
+# echo "$PERSON1:: Hey $PERSON2, How are you?"   		# to differ normal text and variable that why we represent variable with $
+# echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing?"
+# echo "$PERSON1:: I am fine too. What's up?"
+# echo "$PERSON2:: Nothing, Just going to Mars now, will you come"
+# echo "$PERSON1:: Sorry, you carry on! I will come once yu come back"
+# echo "$PERSON2:: Ho thagt's great Idea"
+# echo "$PERSON1:: Sure!!"
