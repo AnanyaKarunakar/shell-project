0d8176fa89a0c4322252e3683521e150c44c5843
AnanyaKarunakar <karunakark1729@gmail.com>
2025-12-09 23:03:06 +0530

    shell-practice

--- shell-practice/05-variables.sh
+++ shell-practice/05-variables.sh
@@ -2,6 +2,6 @@
 
 echo "Enter your pin number::"
 
-read -s PIN #here PIN is variable
+read -s PIN #here PIN is variable, read = reads the input and -s is secure the pin and it wont show here
 
 echo "Your number is: $PIN"
